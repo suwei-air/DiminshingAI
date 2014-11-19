8e707818ef0e355e8a990475821055252531380e
suwei-air <suwei.air@gmail.com>
2014-11-19 23:10:17 +0800

    交换逻辑修改
    在检查交换时就找出可以消除的节点，以及消除方式

--- operation.js
+++ operation.js
@@ -1,36 +1,94 @@
 function check(col, row) {
-  var cur = map[col][row];
+  var cur = map[col][row],
+    result = new Object();
+  result.nodes = new Array();
   if (col>1
     && cur==map[col-2][row]
     && cur==map[col-1][row]) {
-    return true;
+    result.nodes.push({'col': col-2, 'row': row});
+    result.nodes.push({'col': col-1, 'row': row});
+    result.nodes.push({'col': col, 'row': row});
+    result.horizontal = true;
   }
   if (col>0 && col<COLS-1
     && cur==map[col+1][row]
     && cur==map[col-1][row]) {
-    return true;
+    if (result.nodes.length==0) {
+      result.nodes.push({'col': col-1, 'row': row});
+      result.nodes.push({'col': col, 'row': row});
+    }
+    result.nodes.push({'col': col+1, 'row': row});
+    result.horizontal = true;
   }
   if (col<COLS-2
     && cur==map[col+1][row]
     && cur==map[col+2][row]) {
-    return true;
+    if (result.nodes.length==0) {
+      result.nodes.push({'col': col, 'row': row});
+      result.nodes.push({'col': col+1, 'row': row});
+    }
+    result.nodes.push({'col': col+2, 'row': row});
+    result.horizontal = true;
   }
   if (row>1
     && cur==map[col][row-2]
     && cur==map[col][row-1]) {
-    return true;
+    if (result.nodes.length==0) {
+      result.nodes.push({'col': col, 'row': row});
+    }
+    result.nodes.push({'col': col, 'row': row-2});
+    result.nodes.push({'col': col, 'row': row-1});
+    result.vertical = true;
   }
   if (row>0 && row<ROWS-1
     && cur==map[col][row+1]
     && cur==map[col][row-1]) {
-    return true;
+    if (result.nodes.length==0) {
+      result.nodes.push({'col': col, 'row': row});
+    }
+    if (!result.vertical) {
+      result.nodes.push({'col': col, 'row': row-1});
+    }
+    result.nodes.push({'col': col, 'row': row+1});
+    result.vertical = true;
   }
   if (row<ROWS-2
     && cur==map[col][row+1]
     && cur==map[col][row+2]) {
-    return true;
+    if (result.nodes.length==0) {
+      result.nodes.push({'col': col, 'row': row});
+    }
+    if (!result.vertical) {
+      result.nodes.push({'col': col, 'row': row+1});
+    }
+    result.nodes.push({'col': col, 'row': row+2});
+    result.vertical = true;
   }
-  return false;
+  if (
+      (col>1 &&
+        col<COLS-2 &&
+        map[col-2][row]==map[col][row] &&
+        map[col-1][row]==map[col][row] &&
+        map[col+1][row]==map[col][row] &&
+        map[col+2][row]==map[col][row]) ||
+      (col>1 &&
+        col<COLS-2 &&
+        map[col-2][row]==map[col][row] &&
+        map[col-1][row]==map[col][row] &&
+        map[col+1][row]==map[col][row] &&
+        map[col+2][row]==map[col][row])) {
+    result.transformType = '5';
+  }
+  else if (result.horizontal && result.vertical) {
+    result.transformType = 'T';
+  }
+  else if (result.nodes.length==4) {
+    result.transformType = '4';
+  }
+  if (result.transformType) {
+    result.transformNode = {'col': col, 'row': row};
+  }
+  return result.nodes.length>0 ? result : false;
 }
 
 function initCheck(col, row) {
@@ -60,7 +118,7 @@ function initialize() {
   }
 }
 
-function eliminate() {
+function eliminate(checkResult) {
 }
 
 function swap(x, y, direction) {
@@ -93,13 +151,25 @@ function swap(x, y, direction) {
     var temp = map[col][row];
     map[col][row] = map[colDest][rowDest];
     map[colDest][rowDest] = temp;
-    var checkResult = check(col,row) || check(colDest, rowDest);
-    if (!checkResult) {
+    var checkResult,
+      results = new Array();
+    if (map[col][row]!=map[colDest][rowDest]) {
+      if (checkResult = check(col, row)) {
+        results.push(checkResult);
+      }
+      if (checkResult = check(colDest, rowDest)) {
+        results.push(checkResult);
+      }
+    }
+    if (results.length==0) {
       animationSwap(col, row, direction, function() {
         temp = map[col][row];
         map[col][row] = map[colDest][rowDest];
         map[colDest][rowDest] = temp;
       });
+    }
+    else {
+      eliminate(results);
     }
   });
   return true;
